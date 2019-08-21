23b191b5c6528f2b1ec2a8683e6b49c8c6bb44d4
Anas <anas.kaadan@outlook.com>
2019-08-21 18:56:14 +0300

    Removed extra PN from CLI

--- H1BR6/H1BR6.c
+++ H1BR6/H1BR6.c
@@ -85,7 +85,7 @@ portBASE_TYPE resumeCommand( int8_t *pcWriteBuffer, size_t xWriteBufferLen, cons
 const CLI_Command_Definition_t demoCommandDefinition =
 {
 	( const int8_t * ) "demo", /* The command string to type. */
-	( const int8_t * ) "(H1BR6) demo:\r\n Run a demo program to test module functionality\r\n\r\n",
+	( const int8_t * ) "demo:\r\n Run a demo program to test module functionality\r\n\r\n",
 	demoCommand, /* The function to run. */
 	0 /* No parameters are expected. */
 };
@@ -94,7 +94,7 @@ const CLI_Command_Definition_t demoCommandDefinition =
 const CLI_Command_Definition_t addLogCommandDefinition =
 {
 	( const int8_t * ) "addlog", /* The command string to type. */
-	( const int8_t * ) "(H1BR6) addlog:\r\n Add a new log file. Specifiy log name (1st par.); type (2nd par.): 'rate' or 'event'; \
+	( const int8_t * ) "addlog:\r\n Add a new log file. Specifiy log name (1st par.); type (2nd par.): 'rate' or 'event'; \
 rate (3rd par.): logging rate in Hz (max 1000), delimiter format (4th par.): 'space', 'tab' or 'comma'; index column format \
 (5th par.): 'none', 'sample' or 'time'; and index column label text (6th par.)\r\n\r\n",
 	addLogCommand, /* The function to run. */
@@ -105,7 +105,7 @@ rate (3rd par.): logging rate in Hz (max 1000), delimiter format (4th par.): 'sp
 const CLI_Command_Definition_t logVarCommandDefinition =
 {
 	( const int8_t * ) "logvar", /* The command string to type. */
-	( const int8_t * ) "(H1BR6) logvar:\r\n Add a new log variable to an existing log (1st par.). Specify variable type (2nd and 3rd par.): \
+	( const int8_t * ) "logvar:\r\n Add a new log variable to an existing log (1st par.). Specify variable type (2nd and 3rd par.): \
 'port digital', 'port data', 'port button', 'memory uint8', 'memory int8', 'memory uint16', 'memory int16', 'memory uint32', \
 'memory int32', 'memory float' ; source (4th par.): ports 'p1'..'px', buttons 'b1'..'bx' or memory location (Flash or RAM); \
 and column label text (5th par.)\r\n\r\n",
@@ -117,7 +117,7 @@ and column label text (5th par.)\r\n\r\n",
 const CLI_Command_Definition_t deleteLogCommandDefinition =
 {
 	( const int8_t * ) "deletelog", /* The command string to type. */
-	( const int8_t * ) "(H1BR6) deletelog:\r\n Delete a log file. Specifiy log name (1st par.) and delete options (2nd par.): 'all' or \
+	( const int8_t * ) "deletelog:\r\n Delete a log file. Specifiy log name (1st par.) and delete options (2nd par.): 'all' or \
 'keepdisk' to keep log on the uSD card\r\n\r\n",
 	deleteLogCommand, /* The function to run. */
 	2 /* Two parameters are expected. */
@@ -127,7 +127,7 @@ const CLI_Command_Definition_t deleteLogCommandDefinition =
 const CLI_Command_Definition_t startCommandDefinition =
 {
 	( const int8_t * ) "start", /* The command string to type. */
-	( const int8_t * ) "(H1BR6) start:\r\n Start the log with log name (1st par.)\r\n\r\n",
+	( const int8_t * ) "start:\r\n Start the log with log name (1st par.)\r\n\r\n",
 	startCommand, /* The function to run. */
 	1 /* One parameter is expected. */
 };
@@ -136,7 +136,7 @@ const CLI_Command_Definition_t startCommandDefinition =
 const CLI_Command_Definition_t stopCommandDefinition =
 {
 	( const int8_t * ) "stop", /* The command string to type. */
-	( const int8_t * ) "(H1BR6) stop:\r\n Stop the log with log name (1st par.)\r\n\r\n",
+	( const int8_t * ) "stop:\r\n Stop the log with log name (1st par.)\r\n\r\n",
 	stopCommand, /* The function to run. */
 	1 /* One parameter is expected. */
 };
@@ -145,7 +145,7 @@ const CLI_Command_Definition_t stopCommandDefinition =
 const CLI_Command_Definition_t pauseCommandDefinition =
 {
 	( const int8_t * ) "pause", /* The command string to type. */
-	( const int8_t * ) "(H1BR6) pause:\r\n Pause the log with log name (1st par.)\r\n\r\n",
+	( const int8_t * ) "pause:\r\n Pause the log with log name (1st par.)\r\n\r\n",
 	pauseCommand, /* The function to run. */
 	1 /* One parameter is expected. */
 };
@@ -154,7 +154,7 @@ const CLI_Command_Definition_t pauseCommandDefinition =
 const CLI_Command_Definition_t resumeCommandDefinition =
 {
 	( const int8_t * ) "resume", /* The command string to type. */
-	( const int8_t * ) "(H1BR6) resume:\r\n Resume the log with log name (1st par.)\r\n\r\n",
+	( const int8_t * ) "resume:\r\n Resume the log with log name (1st par.)\r\n\r\n",
 	resumeCommand, /* The function to run. */
 	1 /* One parameter is expected. */
 };
